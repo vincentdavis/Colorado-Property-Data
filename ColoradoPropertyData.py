ff360b2795b10807a0dc3870ed217739622bde5d
Vincent Davis <vincent@vincentdavis.net>
2016-01-02 10:38:58 -0700

    basic query

--- ColoradoPropertyData.py
+++ ColoradoPropertyData.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from db import DB, Parcel, Account, LienAuction
 from flask_table import Table, Col
 
@@ -21,12 +21,20 @@ def after_request(response):
 #     name = Col('Name')
 #     description = Col('Description')
 
+
 @app.route('/')
 def hello_world():
+    search_query = request.args.get('q')
+    if search_query:
+        entries = LienAuction.select().where(LienAuction.Tax_Year == search_query)
+    else:
+        entries = LienAuction.select().where(LienAuction.Tax_Year == 2014)
     # sample = [(s.Winning_Bid, s.Face_Value) for s in LienAuction.select().where(LienAuction.Tax_Year == 2014)]
-    entries = LienAuction.select().where(LienAuction.Tax_Year == 2014)
+    #entries = LienAuction.select().where(LienAuction.Tax_Year == 2014)
     #sample = [1,2,3,4,5]
     return render_template('base.html', entries=entries)
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(debug=True)
+
