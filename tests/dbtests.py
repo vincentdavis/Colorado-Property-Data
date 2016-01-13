7426308c94612688a8499b0cb397fde45a1b66f2
Vincent Davis <vincent@vincentdavis.net>
2016-01-13 10:05:10 -0700

    rearrage files

--- tests/dbtests.py
+++ tests/dbtests.py
@@ -1,4 +1,5 @@
 from peewee import *
-DB = MySQLDatabase('codatadb',
+DB = MySQLDatabase('heteroskedastic1$codata',
                    user='heteroskedastic1',
+                   password='Ind5esh8cEv6dy5wOt4Oc4noW',
                    host='heteroskedastic1.mysql.pythonanywhere-services.com')
