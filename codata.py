db5d5757f897911a6e9e77ac628af829be1d2d30
Vincent Davis <vincent@vincentdavis.net>
2016-01-13 10:46:27 -0700

    production db setup

--- codata.py
+++ codata.py
@@ -40,5 +40,5 @@ def search():
 
 if __name__ == '__main__':
     #app.run()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 
--- db.py
+++ db.py
@@ -2,7 +2,12 @@ from peewee import *
 import datetime
 # import pandas as pd
 
-DB = SqliteDatabase('ColoradoPropertyData.db')
+#DB = SqliteDatabase('ColoradoPropertyData.db')
+
+DB = MySQLDatabase('heteroskedastic1$codata',
+                   user='heteroskedastic1',
+                   password='Ind5esh8cEv6dy5wOt4Oc4noW',
+                   host='heteroskedastic1.mysql.pythonanywhere-services.com')
 
 
 ##########################################################################################
